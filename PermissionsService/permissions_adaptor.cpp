27664f862cfbe21a663658f644ad92c43475e206
oleg0 <oleg080505@gmail.com>
2024-11-01 01:35:22 +0300

    add errors

--- PermissionsService/permissions_adaptor.cpp
+++ PermissionsService/permissions_adaptor.cpp
@@ -27,28 +27,32 @@ PermissionsAdaptor::~PermissionsAdaptor()
 void PermissionsAdaptor::RequestPermission(const int32_t &permissionEnumCode)
 {
     auto obj = getObject().getCurrentlyProcessedMessage();
-    std::string path_to_exe;
-    try
+    std::string path_to_exe = [&]()
     {
-        path_to_exe = get_exe_for_pid(obj.getCredsPid());
-    }
-    catch (const std::filesystem::filesystem_error &e)
-    {
-        throw sdbus::Error(sdbus::Error::Name("file error"), "can't find file with given path or smth went wrong with the path.");
-    }
+        try
+        {
+            return get_exe_for_pid(obj.getCredsPid());
+        }
+        catch (const std::filesystem::filesystem_error &e)
+        {
+            throw sdbus::Error(sdbus::Error::Name("com.system.permissions.FileError"), "can't find sender path or something else went wrong.");
+        }
+    }();
     db::Database::Instance().setPermission(path_to_exe, permissionEnumCode);
 }
 bool PermissionsAdaptor::CheckApplicationHasPermission(const std::string &applicationExecPath, const int32_t &permissionEnumCode)
 {
-    std::string path_canonical;
-    try
+    std::string path_canonical = [&]()
     {
-        path_canonical = std::filesystem::canonical(applicationExecPath).string();
-    }
-    catch (const std::filesystem::filesystem_error &e)
-    {
-        throw sdbus::Error(sdbus::Error::Name("file error"), "can't find file with given path or smth went wrong with the path.");
-    }
+        try
+        {
+            return std::filesystem::canonical(applicationExecPath).string();
+        }
+        catch (const std::filesystem::filesystem_error &e)
+        {
+            throw sdbus::Error(sdbus::Error::Name("com.system.permissions.FileError"), "can't find file with given path or something else went wrong.");
+        }
+    }();
 
     return db::Database::Instance().hasPermission(path_canonical, permissionEnumCode);
 }
